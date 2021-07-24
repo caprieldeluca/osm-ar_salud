57bb12316a7e8ec2331b48619b9282d35a5ebe81
gabriel-de-luca <pablogabrieldeluca@gmail.com>
2021-07-24 14:48:41 -0300

    Separar scripts de entorno y consultas para automatizacion

--- automatizacion/consultas.sh
+++ automatizacion/consultas.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-echo "  Iniciar conda ..."
-eval "$(conda shell.bash hook)"
-echo "    ... hecho."
-
-echo "  Activar entorno NodeJS ..."
-conda activate nodejs
-echo "    ... hecho."
-
-echo "  Cambiar directorio de trabajo ..."
-cd /home/gabriel/Proyectos/pruebas/
-echo "    ... hecho."
 
 echo "  Consultar overpass API ..."
-wget -O doctores-osm.json --post-file=consulta-doctores.txt "https://overpass-api.de/api/interpreter"
+wget -O automatizacion/hospitales-osm.json --post-file=automatizacion/consulta-hospitales.txt "https://overpass-api.de/api/interpreter"
+sleep 60
+wget -O automatizacion/clinicas-osm.json --post-file=automatizacion/consulta-clinicas.txt "https://overpass-api.de/api/interpreter"
+sleep 60
+wget -O automatizacion/farmacias-osm.json --post-file=automatizacion/consulta-farmacias.txt "https://overpass-api.de/api/interpreter"
+sleep 60
+wget -O automatizacion/doctores-osm.json --post-file=automatizacion/consulta-doctores.txt "https://overpass-api.de/api/interpreter"
+sleep 60
+wget -O automatizacion/higiene-osm.json --post-file=consulta-higiene.txt "https://overpass-api.de/api/interpreter"
 echo "    ... hecho."
 
 echo "  Convertir json a geojson ..."
-osmtogeojson doctores-osm.json > doctores-osm.geojson
+osmtogeojson automatizacion/hospitales-osm.json > hospitales-osm.geojson
+osmtogeojson automatizacion/clinicas-osm.json > clinicas-osm.geojson
+osmtogeojson automatizacion/farmacias-osm.json > farmacias-osm.geojson
+osmtogeojson automatizacion/doctores-osm.json > doctores-osm.geojson
+osmtogeojson automatizacion/higiene-osm.json > higiene-osm.geojson
 echo "    ... hecho."
 
 echo "  Agregar y confirmar actualización al repositorio local ..."
-git commit -a -m 'Actualizacion 20210721'
+git commit -a -m "Actualizacion $(date +%Y%m%d)"
 echo "    ... hecho."
+
