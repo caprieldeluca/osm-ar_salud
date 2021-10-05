77766d07913d86120ff58d467066fafec5378341
gabriel-de-luca <pablogabrieldeluca@gmail.com>
2021-10-04 23:08:06 -0300

    Actualizacion 20211004

--- automatizacion/consultas.sh
+++ automatizacion/consultas.sh
@@ -3,13 +3,13 @@
 echo "  Consultar overpass API ..."
 wget -O automatizacion/hospitales-osm.json --post-file=automatizacion/consulta-hospitales.txt "https://overpass-api.de/api/interpreter"
 sleep 120
-wget -O automatizacion/clinicas-osm.json --post-file=automatizacion/consulta-clinicas.txt --no-check-certificate --no-cache --no-cookies --no-http-keep-alive "https://overpass-api.de/api/interpreter"
+wget -O automatizacion/clinicas-osm.json --post-file=automatizacion/consulta-clinicas.txt "https://overpass-api.de/api/interpreter"
 sleep 120
-wget -O automatizacion/farmacias-osm.json --post-file=automatizacion/consulta-farmacias.txt --no-check-certificate --no-cache --no-cookies --no-http-keep-alive "https://overpass-api.de/api/interpreter"
+wget -O automatizacion/farmacias-osm.json --post-file=automatizacion/consulta-farmacias.txt "https://overpass-api.de/api/interpreter"
 sleep 120
-wget -O automatizacion/doctores-osm.json --post-file=automatizacion/consulta-doctores.txt --no-check-certificate --no-cache --no-cookies --no-http-keep-alive "https://overpass-api.de/api/interpreter"
+wget -O automatizacion/doctores-osm.json --post-file=automatizacion/consulta-doctores.txt "https://overpass-api.de/api/interpreter"
 sleep 120
-wget -O automatizacion/higiene-osm.json --post-file=automatizacion/consulta-higiene.txt --no-check-certificate --no-cache --no-cookies --no-http-keep-alive "https://overpass-api.de/api/interpreter"
+wget -O automatizacion/higiene-osm.json --post-file=automatizacion/consulta-higiene.txt "https://overpass-api.de/api/interpreter"
 echo "    ... hecho."
 
 echo "  Convertir json a geojson ..."
