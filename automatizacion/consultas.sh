27499c7ab3befcc1a2ae9a5d47e0463c355bfd32
gabriel-de-luca <pablogabrieldeluca@gmail.com>
2021-10-07 00:44:17 -0300

    Actualizacion 20211007

--- automatizacion/consultas.sh
+++ automatizacion/consultas.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-echo "  Consultar overpass API ..."
-wget -O automatizacion/hospitales-osm.json --post-file=automatizacion/consulta-hospitales.txt "https://overpass-api.de/api/interpreter"
-sleep 120
-wget -O automatizacion/clinicas-osm.json --post-file=automatizacion/consulta-clinicas.txt "https://overpass-api.de/api/interpreter"
-sleep 120
-wget -O automatizacion/farmacias-osm.json --post-file=automatizacion/consulta-farmacias.txt "https://overpass-api.de/api/interpreter"
-sleep 120
+echo "  Consultar overpass API  y convertir a geojson..."
+if wget -O automatizacion/hospitales-osm.json --post-file=automatizacion/consulta-hospitales.txt "https://overpass-api.de/api/interpreter" && jsonlint automatizacion/hospitales-osm.json; then
+    osmtogeojson automatizacion/hospitales-osm.json > hospitales-osm.geojson
+fi
+sleep 10
+if wget -O automatizacion/clinicas-osm.json --post-file=automatizacion/consulta-clinicas.txt "https://overpass-api.de/api/interpreter" && jsonlint automatizacion/clinicas-osm.json; then
+    osmtogeojson automatizacion/clinicas-osm.json > clinicas-osm.geojson
+fi
+sleep 10
+if wget -O automatizacion/farmacias-osm.json --post-file=automatizacion/consulta-farmacias.txt "https://overpass-api.de/api/interpreter" && jsonlint automatizacion/farmacias-osm.json; then
+    osmtogeojson automatizacion/farmacias-osm.json > farmacias-osm.geojson
+fi
+sleep 10
+if wget -O automatizacion/doctores-osm.json --post-file=automatizacion/consulta-doctores.txt "https://overpass-api.de/api/interpreter" && jsonlint automatizacion/doctores-osm.json; then
+    osmtogeojson automatizacion/doctores-osm.json > doctores-osm.geojson
+fi
 wget -O automatizacion/doctores-osm.json --post-file=automatizacion/consulta-doctores.txt "https://overpass-api.de/api/interpreter"
-sleep 120
-wget -O automatizacion/higiene-osm.json --post-file=automatizacion/consulta-higiene.txt "https://overpass-api.de/api/interpreter"
-echo "    ... hecho."
-
-echo "  Convertir json a geojson ..."
-osmtogeojson automatizacion/hospitales-osm.json > hospitales-osm.geojson
-osmtogeojson automatizacion/clinicas-osm.json > clinicas-osm.geojson
-osmtogeojson automatizacion/farmacias-osm.json > farmacias-osm.geojson
-osmtogeojson automatizacion/doctores-osm.json > doctores-osm.geojson
-osmtogeojson automatizacion/higiene-osm.json > higiene-osm.geojson
+sleep 10
+if wget -O automatizacion/higiene-osm.json --post-file=automatizacion/consulta-higiene.txt "https://overpass-api.de/api/interpreter" && jsonlint automatizacion/higiene-osm.json; then
+    osmtogeojson automatizacion/higiene-osm.json > higiene-osm.geojson
+fi
 echo "    ... hecho."
 
 echo "  Mostrar status resumido ..."
